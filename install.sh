a54fe929e18c85e231b53625f8240b512d752c0a
robaldis <robertaldis@gmail.com>
2021-06-03 19:09:56 +0100

    adding a few install commands

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+
+sudo apt install -y keepassx
+sudo apt install -y git
+sudo apt install -y brave-browser
+sudo apt install -y tmux
+
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install -y neovim
+
+
