7d0ef629ef90da59e25ac592604be04f018a3581
robaldis <robertaldis@gmail.com>
2022-01-06 11:18:27 +0000

    adding only office

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 
 sudo apt install -y keepassx tmux curl i3 zsh owncloud-client feh
 
+# installing only office desktop
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+sudo apt update
+sudo apt install onlyoffice-desktopeditors
+
 # isntalling brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
