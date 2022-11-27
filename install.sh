d2618d093a4e1ece945f132054675b302bda7d3b
robaldis <robertaldis@gmail.com>
2022-11-27 11:00:58 +0000

    adding all the things

--- install.sh
+++ install.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-sudo apt install -y keepassx tmux curl i3 zsh owncloud-client feh i3blocks rofi fzf
+# Prerequisites
+sudo apt install -y ninja-build gettext libtool libtool-bin autoconf automake\
+    cmake g++ pkg-config unzip curl doxygen
 
-# installing only office desktop
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+# tools
+sudo apt install -y keepassx tmux curl i3 zsh  feh i3blocks rofi\
+    fzf stow cura
+
+# Owncloud (This seems to be version specific for some fucking reason)
+wget -nv https://download.owncloud.com/desktop/ownCloud/stable/2.10/linux/Ubuntu_22.04/Release.key -O - | sudo apt-key add -
+echo 'deb https://download.owncloud.com/desktop/ownCloud/stable/2.10/linux/Ubuntu_22.04/ /' | sudo tee -a /etc/apt/sources.list.d/owncloud.list
 sudo apt update
-sudo apt install onlyoffice-desktopeditors
+sudo apt install -y owncloud-client
+
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/spotify.gpg
+echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt -y install spotify-client
+
+# installing only office desktop (No longer works)
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+#sudo apt update
+#sudo apt install onlyoffice-desktopeditors
 
 # isntalling brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -14,34 +32,23 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 sudo apt update
 sudo apt install -y brave-browser
 
-# installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# You have to press enter for this to install
-cd Documents
+# install neovim
+pushd ~/Documents
 git clone https://github.com/neovim/neovim
 cd neovim
-git checkout release-0.7
+git checkout stable
 sudo make CMAKE_BUILD_TYPE=Release
 sudo make install
-cd ../..
+popd
 
-cd ~
+# Install plugged for neovim package manager
+pushd ~
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+popd
 
-# sudo add-apt-repository -y ppa:neovim-ppa/unstable
-# sudo apt update
-# sudo apt install -y neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "Do you wish to install all config files?"
-
-read installConfigs
-
-if $installConfigs == "y"
-then
-	# run symlink script
-    ./symlink
-fi
+# installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- symlink.sh
+++ symlink.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-echo "Removing existing config files/directories"
 
-# Remove all the old config files
-rm -rf ~/.zshrc
-rm -rf ~/.profile
-rm -rf ~/.gitconfig
-rm -rf ~/.tmux.conf
-rm -rf ~/.config/nvim
-rm -rf ~/.config/i3
-rm -rf ~/.config/compton.conf
-rm -rf ~/.Xresources
-rm -rf ~/.fonts
-
-echo "Linking the new config files"
-
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.profile ~/
-ln -s ~/.dotfiles/.gitconfig ~/
-ln -s ~/.dotfiles/.tmux.conf ~/
-ln -s ~/.dotfiles/.config/nvim/ ~/.config/
-ln -s ~/.dotfiles/.config/i3/ ~/.config/
-ln -s ~/.dotfiles/.config/compton.conf ~/.config/
-ln -s ~/.dotfiles/.Xresources ~/
-ln -s ~/.dotfiles/.fonts ~/
-ln -s ~/.dotfiles/real-prog-qwerty /usr/share/X11/xkb/symbols/
-ln -s ~/.dotfiles/.local/bin/tmux-sessionizer ~/.local/bin/
 
 # Making zsh defulat terminal
 sudo chsh -s /bin/zsh
