49f75898f72e605183cc205b6886385924afc1b3
robaldis <robertaldis@gmail.com>
2022-11-28 20:11:10 +0000

    adding link with stow

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+STOW_FOLDERS="bin,i3,nvim,tmux,zsh,scripts"
+for foles in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do 
+    echo "stow $folder"
+    stow -D $folder
+    stow $folder
 
 # Making zsh defulat terminal
 sudo chsh -s /bin/zsh
