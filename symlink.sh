0b3f2e37688d94eb5e1a0ee021f220c5abf5fb9a
robaldis <robertaldis@gmail.com>
2022-11-28 20:16:49 +0000

    corrections

--- symlink.sh
+++ symlink.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-STOW_FOLDERS="bin,i3,nvim,tmux,zsh,scripts"
-for foles in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+STOW_FOLDERS="i3,nvim,tmux,zsh,scripts"
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do 
     echo "stow $folder"
     stow -D $folder
-    stow $folder
-
+done
+    
 # Making zsh defulat terminal
 sudo chsh -s /bin/zsh
