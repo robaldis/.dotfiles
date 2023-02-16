50a43cd59220c26c528d6db1ba7b53a9cb0681bd
robert.aldis <robert.aldis@finbourne.com>
2023-02-16 10:16:11 +0000

    Adding quickfix options

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -30,3 +30,6 @@ vnoremap("<leader>y", "\"+y")
 
 
 nnoremap("<C-n>", "<cmd>NERDTreeToggle<CR>")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
